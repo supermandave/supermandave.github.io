bf133a2bb9566659486aa8d017ecb0ef44a6ccc5
David Mills <dam203@txstate.edu>
2019-09-19 14:07:43 -0500

    Completed Task 5. Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -5,8 +5,17 @@ const out = x + y;
 document.write(out);
 */
 
-x = 22;
-const y = 33;
-document.writeln(x + y);
-document.writeln("<br>");
-document.writeln(x += 33);
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+
+function condition()
+{
+  x = confirm("Are you sure you want to proceed?");
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+  {
+    document.writeln("You chose Cancel!");
+  }
+}
