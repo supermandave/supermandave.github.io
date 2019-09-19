835c19a7a6b4f9b922742b9b78bada4395fe07f6
David Mills <dam203@txstate.edu>
2019-09-19 13:41:38 -0500

    Completed Task 4. Operators

--- javascript/external.js
+++ javascript/external.js
@@ -5,6 +5,8 @@ const out = x + y;
 document.write(out);
 */
 
-// A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+x = 22;
+const y = 33;
+document.writeln(x + y);
+document.writeln("<br>");
+document.writeln(x += 33);
